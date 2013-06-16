cf4c012d273e77d819936220cd7000778a8126eb
johnsmythe <mlikj2006@gmail.com>
2013-06-16 19:46:18 -0400

    new commit

--- music/musicapp/app/controllers/home_controller.rb
+++ music/musicapp/app/controllers/home_controller.rb
@@ -3,13 +3,16 @@ class HomeController < ApplicationController
 	session[:link_list] ||= []
 	@position = 0
 
-        if params[:add] && params[:add] != ""
-		puts "adding"
-                session[:link_list].append(params[:add])
+        if params[:add] 
+		if  params[:add] != ""
+                	session[:link_list].append(params[:add])
+			redirect_to root_path
+		end
 		
 
         elsif params[:submit]
 		session[:link_list].delete_at(params[:submit].to_i)
+		redirect_to root_path
 	end
   end
   
